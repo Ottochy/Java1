96b8d4b51cc667e604f1dcaa34449bab171efb0f
Ottochy <102737499+Ottochy@users.noreply.github.com>
2022-05-05 10:51:29 -0300

    Add files via upload

---
+++ Exercicio-dois.js
@@ -0,0 +1,33 @@
+//1
+
+function nome(x){
+ return console.log(x)
+
+}
+
+nome('Otavio')
+
+//2
+
+function soma(a,b){
+    return console.log(a + b)
+}
+
+soma(1,2)
+
+//3
+
+function multi(a,b){
+    return console.log(a * b)
+}
+
+multi(4,6)
+
+//4
+
+function media(a,b,c,d){
+    let res = (a+b+c+d)/4
+    return console.log(res)
+}
+
+media(9,8,4,3)
